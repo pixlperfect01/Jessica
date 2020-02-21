8c4fe49d8ef2c1a98cf4d44b70022cc0d05152fa
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2020-02-21 09:44:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var c = document.getElementsByTagName("CANVAS")[0];
-c.width=document.innerWidth;
-c.height=document.innerHeight;
+c.width=window.innerWidth;
+c.height=window.innerHeight;
 var ctx = c.getContext("2d");
 ctx.fillColor="#000000";
 ctx.fillRect(0,0,c.width, c.height);
